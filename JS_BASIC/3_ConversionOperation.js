de9d23c2abbe63f643b6eb3a6f54729e03e6e81c
UrvishaSavaliya <82795619+UrvishaSavaliya@users.noreply.github.com>
2024-09-13 10:04:20 +0000

    conversion of datatype

---
+++ JS_BASIC/3_ConversionOperation.js
@@ -0,0 +1,25 @@
+//let score = 33
+
+// let score = "33"
+
+let score = "33abc";
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+//1 , "" , "abc"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
