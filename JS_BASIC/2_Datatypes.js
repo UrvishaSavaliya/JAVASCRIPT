01af6d80bfe1529b4338fe79e781dd202590a536
UrvishaSavaliya <82795619+UrvishaSavaliya@users.noreply.github.com>
2024-09-13 09:12:39 +0000

    datatypes

---
+++ JS_BASIC/2_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict";   //treat all js code as newer version
+
+//alert(50+25);        //not run because run on node
+
+
+//numer =>  2 to power 53
+//bigint
+//string    =>  ""
+//boolean   =>  true/false
+//null  =>  standalone value
+//undefined     =>  
+//symbol    =>  unique
+
+//object
+
+console.log(typeof "Abc");
+
+console.log(typeof null)    //type of object
+
+console.log(typeof undefined);  //type of undefined
