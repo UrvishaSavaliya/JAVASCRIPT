b54edb7076226bca2a7f76d1416627d060036361
UrvishaSavaliya <82795619+UrvishaSavaliya@users.noreply.github.com>
2024-09-13 10:24:19 +0000

    conversion operation

--- JS_BASIC/3_ConversionOperation.js
+++ JS_BASIC/3_ConversionOperation.js
@@ -23,3 +23,32 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+//***********************************   Operations  *****************************************************************
+
+let value = 3
+let negvalue = -value
+
+console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Good morning"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log((3+4) * (5%3));
+
+console.log(+true)  //1
+
+console.log(+"")
+
+let gameCounter = 100
+++gameCounter;              //solve in mdn
+console.log(gameCounter);
